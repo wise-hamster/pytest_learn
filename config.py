01303bb78ac3dc486bd442a1ba9cc1c4fa732c5c
VA <169175519+wise-hamster@users.noreply.github.com>
2025-06-04 23:32:04 +0300

    Kotik

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 SERVICE_URL = "https://api.spacexdata.com/v3/info"
+LANDING_PADS_URL = 'https://api.spacexdata.com/v3/landpads'
--- tests/test_some.py
+++ tests/test_some.py
@@ -5,10 +5,6 @@ from src.baseclasses.response import Response
 from src.schemas.get import GET_SCHEMA
 from config import SERVICE_URL
 
-print(Response)
-print(GET_SCHEMA)
-print(SERVICE_URL)
-
 def test_getting_posts():
     r = requests.get(url = SERVICE_URL)
     response = Response(r)
@@ -17,3 +13,4 @@ def test_getting_posts():
     response.assert_len_response(15)
     response.assert_ceo('Elon Musk')
 
+def test_ge
